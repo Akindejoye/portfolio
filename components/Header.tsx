52aa4ad78bf8ec639b1b5b49ef910aa8ace105e2
Akindejoye <tydruhill@gmail.com>
2022-11-09 00:30:56 +0100

    Header

---
+++ components/Header.tsx
@@ -0,0 +1,72 @@
+import { SocialIcon } from "react-social-icons";
+import { motion } from 'framer-motion';
+
+const Header = () => {
+  return ( 
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto xl:items-center z-20">
+      <motion.div
+        initial ={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }} 
+        className="flex flex-row items-center"
+      >
+        {/* Socail Icons */}
+        <SocialIcon 
+          network="youtube"
+          fgColor="gray"
+          bgColor="transparent" 
+          // style={{ height: 25, width: 25 }} 
+        />
+        <SocialIcon 
+          network="twitter"
+          fgColor="gray"
+          bgColor="transparent" 
+          // style={{ height: 25, width: 25 }} 
+        />
+        <SocialIcon 
+          network="linkedin"
+          fgColor="gray"
+          bgColor="transparent" 
+          // style={{ height: 25, width: 25 }} 
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{ duration: 1.5 }} 
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          className="cursor-pointer" 
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
+      </motion.div>
+    </header>
+   );
+}
+ 
+export default Header;
